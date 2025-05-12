23f66a621a1a29b847aec9b6f0d78914cdc5feba
MiguelToller <luizmigueltoller@gmail.com>
2025-05-12 11:09:14 -0300

    Update Project

--- SpringThymeleafCRUD/src/main/java/miguel/springthymeleafcrud/controller/ProdutoController.java
+++ SpringThymeleafCRUD/src/main/java/miguel/springthymeleafcrud/controller/ProdutoController.java
@@ -23,14 +23,34 @@ public class ProdutoController {
     }
 
     @PostMapping("/salvar")
-    public @ResponseBody String salvar(@ModelAttribute Produto produto) {
+    public String salvar(@ModelAttribute Produto produto) {
         produtoRepository.save(produto);
-        return "salvo";
+        return "redirect:/produto/listar";
     }
 
     @GetMapping("/listar")
     public String listar(Model model) {
         model.addAttribute("produtos", produtoRepository.findAll());
         return "lista";
+    }
+
+    //Método para editar um produto
+    @GetMapping("/editar/{id}")
+    public String formEditar(@PathVariable int id, Model model) {
+        //Cria um novo objeto do tipo produto com as informações do banco e do id passado
+        Produto produto = produtoRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Produto Inválido: " + id));
+        //Adiciona o objeto produto criado ao modelo (pagina html)
+        model.addAttribute("produto", produto);
+        return "form";
+    }
+
+    //Método para deletar um produto
+    @GetMapping("/deletar/{id}")
+    public String deletar(@PathVariable int id) {
+        Produto produto = produtoRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Produto Inválido: " + id));
+        produtoRepository.delete(produto);
+        return "redirect:/produto/listar";
     }
 }
